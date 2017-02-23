02d8410e7d1832eac53f47fe96d9080634968c88
Nikhil <nnadkarny@gmail.com>
2017-02-23 17:11:19 -0500

    Track 3.8.28 - More validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 4, too_short: "must be at least 4 characters" }, uniqueness: true
+  validates :address, presence: true
+  validates :description, presence: true
 end
